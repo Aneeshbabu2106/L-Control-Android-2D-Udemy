7e8a7db77ed05a232c23c4c6122eff7fe225d2b8
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2022-03-30 08:35:12 +0530

    removing debug logs

--- Assets/Scripts/Accelerometer.cs
+++ Assets/Scripts/Accelerometer.cs
@@ -10,9 +10,6 @@ public class Accelerometer : MonoBehaviour
        float x = Input.acceleration.x;
        float z = Input.acceleration.z;
 
-       Debug.Log("x"+x);
-       Debug.Log("z"+z);
-
        transform.Rotate(0,0,-x*rotationSpeed*Time.deltaTime);
        transform.Translate(0,0,-z*speed*Time.deltaTime);
     }
